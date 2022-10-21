b67e6af873046129c98e00c6d43b40fc2bafae58
sirisha1402 <ysirisha1402@gmail.com>
2022-10-21 16:19:00 +0530

    Add project files.

---
+++ Models/angular_appContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace angular_API.Models
+{
+    public partial class angular_appContext : DbContext
+    {
+        public angular_appContext()
+        {
+        }
+
+        public angular_appContext(DbContextOptions<angular_appContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Registration> Registrations { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+               // optionsBuilder.UseSqlServer("Server=ELW5109\\SQLEXPRESS;Database=angular_app;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Registration>(entity =>
+            {
+                entity.ToTable("Registration");
+
+                //entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Mobile)
+                    .HasMaxLength(25)
+                    .IsUnicode(false)
+                    .HasColumnName("mobile");
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(25)
+                    .IsUnicode(false)
+                    .HasColumnName("password");
+
+                entity.Property(e => e.Useremail)
+                    .HasMaxLength(40)
+                    .IsUnicode(false)
+                    .HasColumnName("useremail");
+
+                entity.Property(e => e.Username)
+                    .HasMaxLength(25)
+                    .IsUnicode(false)
+                    .HasColumnName("username");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+using angular_API.Models;
+using Microsoft.EntityFrameworkCore;
+namespace angular_API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<angular_appContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("TEConnection")));
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+
+
+            var app = builder.Build();
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
